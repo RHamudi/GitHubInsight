3fdc95e64ecf5e71aab40afda334b0932a264ef8
RHamudi <ramonramos.silva16@gmail.com>
2025-08-25 09:06:57 -0300

    feat: start api project

---
+++ src/GithubInsight.API/Program.cs
@@ -0,0 +1,32 @@
+using GithubInsight.Application.Services.APIGithub;
+using GithubInsight.Application.Services.APIGithub.Interfaces;
+using GithubInsight.Infrastructure.Shared.Context;
+
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers();
+builder.Services.AddScoped<GithubInsightContext>();
+builder.Services.AddScoped<IGithubService, GithubService>();
+builder.Services.AddHttpClient();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
+
