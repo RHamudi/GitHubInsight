559e3222a50b8c652d1f5883d8d00f898e342f63
RHamudi <ramonramos.silva16@gmail.com>
2025-08-26 20:50:16 -0300

    feat: query adding stat and user in db

---
+++ src/GithubInsight.Infrastructure/Repositories/StatsRepository.cs
@@ -0,0 +1,26 @@
+﻿using GithubInsight.Infrastructure.Entities;
+using GithubInsight.Infrastructure.Repositories.Interfaces;
+using GithubInsight.Infrastructure.Shared.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubInsight.Infrastructure.Repositories
+{
+    public class StatsRepository : IStatsRepository
+    {
+        private readonly GithubInsightContext _context;
+        public StatsRepository(GithubInsightContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddStatsAsync(Stat stat)
+        {   
+            await _context.Stats.AddAsync(stat);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ src/GithubInsight.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,32 @@
+﻿using GithubInsight.Infrastructure.Entities;
+using GithubInsight.Infrastructure.Repositories.Interfaces;
+using GithubInsight.Infrastructure.Shared.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubInsight.Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly GithubInsightContext _context;
+        public UserRepository(GithubInsightContext context)
+        {
+            _context = context;
+        }
+        public async Task<User> AddUserAsync(User user)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task<bool> UserExistsAsync(string username)
+        {
+            return await _context.Users.AnyAsync(u => u.Login == username);
+        }
+    }
+}
