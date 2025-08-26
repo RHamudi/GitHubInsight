f141c689767fc834d25ed0c674097ffceda92576
RHamudi <ramonramos.silva16@gmail.com>
2025-08-26 20:48:06 -0300

    feat: interfaces to user & stats repository

---
+++ src/GithubInsight.Infrastructure/Repositories/Interfaces/IStatsRepository.cs
@@ -0,0 +1,14 @@
+﻿using GithubInsight.Infrastructure.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubInsight.Infrastructure.Repositories.Interfaces
+{
+    public interface IStatsRepository
+    {
+        Task AddStatsAsync(Stat stat);
+    }
+}
---
+++ src/GithubInsight.Infrastructure/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using GithubInsight.Infrastructure.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubInsight.Infrastructure.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User> AddUserAsync(User user);
+        Task<bool> UserExistsAsync(string username);
+    }
+}
