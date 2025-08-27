7c8ad0cbb663b4da333d843f254c9b781246e76d
RHamudi <ramonramos.silva16@gmail.com>
2025-08-26 21:18:42 -0300

    feat: mapping entity to dto

---
+++ src/GithubInsight.Application/Mappers/StatsMapper.cs
@@ -0,0 +1,43 @@
+﻿using GithubInsight.Application.DTO;
+using GithubInsight.Infrastructure.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubInsight.Application.Mappers
+{
+    internal class StatsMapper
+    {
+        public static StatDto ToDto(Stat stat)
+        {
+            return new StatDto
+            {
+                UserId = stat.UserId,
+                TotalRepos = stat.TotalRepos,
+                Stars = stat.Stars,
+                MostUsedLanguage = stat.MostUsedLanguage,
+                LanguageStats = stat.LanguageStats?.Select(ls => new LanguageStatDto
+                {
+                    
+                    Language = ls.Language,
+                    Count = ls.Count,
+                    CreatedAt = ls.CreatedAt
+                }).ToList() ?? [],
+                ReposCreatedPerYears = stat.ReposCreatedPerYears?.Select(r => new ReposCreatedPerYearDto
+                {
+                    Year = r.Year,
+                    Count = r.Count,
+                    CreatedAt = r.CreatedAt
+                }).ToList() ?? [],
+                TopRepos = stat.TopRepos?.Select(tr => new TopRepoDto
+                {
+                    RepoName = tr.RepoName,
+                    Stars = tr.Stars,
+                    CreatedAt = tr.CreatedAt
+                }).ToList() ?? []
+            };
+        }
+    }
+}
---
+++ src/GithubInsight.Application/Mappers/UserMapper.cs
@@ -0,0 +1,40 @@
+﻿using GithubInsight.Application.DTO;
+using GithubInsight.Infrastructure.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubInsight.Application.Mappers
+{
+    internal class UserMapper
+    {
+        public static User ToEntity(GitHubUserResponse dto)
+        {
+            return new User
+            {
+                Login = dto.Login,
+                Name = dto.Name,
+                Location = dto.Location,
+                CreatedAt = dto.CreatedAt,
+                UpdatedAt = dto.UpdatedAt,
+            };
+        }
+
+        public static UserDTO ToDto(User entity, Stat stat)
+        {
+            if (entity == null) return null;
+
+            return new UserDTO
+            {
+                Login = entity.Login,
+                Name = entity.Name,
+                Location = entity.Location,
+                CreatedAt = entity.CreatedAt,
+                UpdatedAt = entity.UpdatedAt,
+                Stat = StatsMapper.ToDto(stat)
+            };
+        }
+    }
+}
