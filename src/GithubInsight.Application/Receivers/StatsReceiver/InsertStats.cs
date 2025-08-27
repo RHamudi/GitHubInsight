a326ec001cd5f591e022fe949cc97f3e428bf02b
RHamudi <ramonramos.silva16@gmail.com>
2025-08-26 21:19:55 -0300

    feat: implements processing and insertion of repository statistics

---
+++ src/GithubInsight.Application/Receivers/StatsReceiver/InsertStats.cs
@@ -0,0 +1,38 @@
+﻿using GithubInsight.Application.DTO;
+using GithubInsight.Application.Mappers;
+using GithubInsight.Application.Services.APIGithub.Interfaces;
+using GithubInsight.Application.Services.Stats;
+using GithubInsight.Infrastructure.Entities;
+using GithubInsight.Infrastructure.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubInsight.Application.Receivers.RepoReceiver
+{
+    public class InsertStats
+    {
+        private readonly IGithubService _githubService;
+        private readonly IStatsRepository _statsRepository;
+        public InsertStats(IGithubService githubService, IStatsRepository statsRepository)
+        {
+            _githubService = githubService;
+            _statsRepository = statsRepository;
+        }
+
+        public async Task<Stat> ExecuteAsync(string username, int userId)
+        {
+            var repos = await _githubService.GetReposUserAsync(username);
+            if (repos == null || !repos.Any())
+                throw new ArgumentException("Nenhum repositório encontrado para o usuário.");
+
+            var stat = StatsProcessor.Process(repos, userId);
+
+            await _statsRepository.AddStatsAsync(stat);
+
+            return stat;
+        }
+    }
+}
