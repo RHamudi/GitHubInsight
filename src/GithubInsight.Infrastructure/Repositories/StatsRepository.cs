10985f4bbb2e9b8adf748da43fc52fdf99801ae9
RHamudi <ramonramos.silva16@gmail.com>
2025-08-27 21:52:23 -0300

    feat: create new query find stat by user

--- src/GithubInsight.Infrastructure/Repositories/StatsRepository.cs
+++ src/GithubInsight.Infrastructure/Repositories/StatsRepository.cs
@@ -1,6 +1,7 @@
 ﻿using GithubInsight.Infrastructure.Entities;
 using GithubInsight.Infrastructure.Repositories.Interfaces;
 using GithubInsight.Infrastructure.Shared.Context;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,6 +22,18 @@ namespace GithubInsight.Infrastructure.Repositories
         {   
             await _context.Stats.AddAsync(stat);
             await _context.SaveChangesAsync();
+        }
+
+        public async Task<Stat> GetStatByUserAsync(string userLogin)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Login == userLogin) ?? throw new Exception("User not found");
+            var stat = await _context.Stats
+                .Include(s => s.LanguageStats)
+                .Include(s => s.ReposCreatedPerYears)
+                .Include(s => s.TopRepos)
+                .FirstOrDefaultAsync(s => s.UserId == user.Id);
+
+            return stat;
         }
     }
 }
