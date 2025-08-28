0dd43dd875c55caf7c8739eb57aae19fe6e3c80d
RHamudi <ramonramos.silva16@gmail.com>
2025-08-27 22:13:15 -0300

    feat: creating new routes SyncUser/GetStat/ GetTopRepos/GetLanguages

--- src/GithubInsight.API/Controllers/GithubInsightController.cs
+++ src/GithubInsight.API/Controllers/GithubInsightController.cs
@@ -2,6 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GithubInsight.Application.Mappers;
+using GithubInsight.Application.Receivers.LanguageReceiver;
+using GithubInsight.Application.Receivers.StatsReceiver;
+using GithubInsight.Application.Receivers.TopReposReceiver;
 using GithubInsight.Application.Receivers.UserReceiver;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,14 +16,23 @@ namespace GithubInsight.API.Controllers
     public class GithubInsightController : ControllerBase
     {
         private readonly InsertUser _insertReceiver;
+        private readonly ReadStats _readStatsReceiver;
+        private readonly ReadTopRepos _readTopReposReceiver;
+        private readonly ReadLanguage _readLanguage;
 
-        public GithubInsightController(InsertUser insertReceiver)
+        public GithubInsightController(InsertUser insertReceiver, 
+            ReadStats readStatsReceiver, 
+            ReadTopRepos readTopRepos,
+            ReadLanguage readLanguage)
         {
             _insertReceiver = insertReceiver;
+            _readStatsReceiver = readStatsReceiver;
+            _readTopReposReceiver = readTopRepos;
+            _readLanguage = readLanguage;
         }
 
         [HttpGet("sync-user/{username}")]
-        public IActionResult GetStatus(string username)
+        public IActionResult SyncUser(string username)
         {
             try
             {
@@ -42,6 +55,76 @@ namespace GithubInsight.API.Controllers
             catch (Exception ex)
             {
                 return StatusCode(500, new { message = "Internal server error", error = ex.Message });
+            }
+        }
+
+        [HttpGet("stats/{username}")]
+        public IActionResult GetStat(string username)
+        {
+            try
+            {
+                var stat = _readStatsReceiver.ExecuteAsync(username).Result;
+
+                if (stat.StatusCode == 200)
+                {
+                    return Ok(new
+                    {
+                        message = stat.Message,
+                        stats = stat.Data
+                    });
+                }
+
+                return StatusCode(stat.StatusCode, new
+                {
+                    message = stat.Message,
+                });
+            } catch (Exception ex)
+            {
+                return StatusCode(500, new { message = "Internal server error", error = ex.Message });
+            }
+        }
+
+        [HttpGet("TopRepos/{username}")]
+        public IActionResult GetTopRepos(string username) {
+            try
+            {
+                var topRepos = _readTopReposReceiver.ExecuteAsync(username).Result;
+
+                if (topRepos.StatusCode == 200)
+                {
+                    return Ok(topRepos);
+
+                }
+
+                return StatusCode(topRepos.StatusCode, new
+                {
+                    message = topRepos.Message,
+                });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = "Internal server error", error = ex.Message });
+            }
+        }
+
+        [HttpGet("Languages/{username}")]
+        public IActionResult GetLangugages(string username) {
+            try
+            {
+                var languages = _readLanguage.ExecuteAsync(username).Result;
+
+                if(languages.StatusCode == 200)
+                {
+                    return Ok(languages);
+                }
+
+                return StatusCode(languages.StatusCode, new
+                {
+                    message = languages.Message,
+                });
+            } catch (Exception ex)
+            {
+                return StatusCode(500, new {message = "Internal server error", error = ex.Message});
             }
         }
     }
