78d8c5443f662c20b8128aa6cf004f6e221c8709
RHamudi <ramonramos.silva16@gmail.com>
2025-08-27 21:53:29 -0300

    feat: new repository get all languages

---
+++ src/GithubInsight.Infrastructure/Repositories/LanguagesRepository.cs
@@ -0,0 +1,30 @@
+﻿using GithubInsight.Infrastructure.Entities;
+using GithubInsight.Infrastructure.Repositories.Interfaces;
+using GithubInsight.Infrastructure.Shared.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubInsight.Infrastructure.Repositories
+{
+    public class LanguagesRepository : ILanguagesRepository
+    {
+        private readonly GithubInsightContext _context;
+
+        public LanguagesRepository(GithubInsightContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<LanguageStat>> GetAllLanguagesByUser(string userLogin)
+        {
+            return await _context.LanguageStats
+                .Where(ls => ls.Stats.User.Login == userLogin)
+                .OrderByDescending(tr => tr.Count)
+                .ToListAsync();
+        }
+    }
+}
