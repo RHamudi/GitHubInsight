72a0d57227b458033e4156fe69d0ba1b43136511
RHamudi <ramonramos.silva16@gmail.com>
2025-08-27 21:57:41 -0300

    feat: DTO to response api

---
+++ src/GithubInsight.Application/DTO/LanguageDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubInsight.Application.DTO
+{
+    internal class LanguageDTO
+    {
+        public string Language { get; set; } = null!;
+
+        public int Count { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+    }
+}
---
+++ src/GithubInsight.Application/DTO/TopReposDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubInsight.Application.DTO
+{
+    internal class TopReposDTO
+    {
+        public string RepoName { get; set; } = null!;
+
+        public int Stars { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+    }
+}
