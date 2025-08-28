930fe5b2bedac4b15cc020157837cf3b1dcd40b3
RHamudi <ramonramos.silva16@gmail.com>
2025-08-27 21:53:12 -0300

    feat: new repository find top repos in db

---
+++ src/GithubInsight.Infrastructure/Repositories/TopReposRepository.cs
@@ -0,0 +1,29 @@
+﻿using GithubInsight.Infrastructure.Entities;
+using GithubInsight.Infrastructure.Repositories.Interfaces;
+using GithubInsight.Infrastructure.Shared.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubInsight.Infrastructure.Repositories
+{
+    public class TopReposRepository : ITopReposRepository
+    {
+        private readonly GithubInsightContext _context;
+        public TopReposRepository(GithubInsightContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<TopRepo>> GetTopReposAsync(string userLogin)
+        {
+            return await _context.TopRepos
+                .Where(tr => tr.Stats.User.Login == userLogin)
+                .OrderByDescending(tr => tr.Stars)
+                .ToListAsync();
+        }
+    }
+}
